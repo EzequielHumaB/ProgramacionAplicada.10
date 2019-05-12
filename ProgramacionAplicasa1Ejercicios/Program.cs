44367b8c86a24fe144b79ed11511417b5f443b38
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-05-11 21:02:50 -0400

    COmm

---
+++ ProgramacionAplicasa1Ejercicios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramacionAplicasa1Ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
